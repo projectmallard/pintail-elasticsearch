34f2db958af6078b337007a673d06698498e81a7
Shaun McCance <shaunm@redhat.com>
2016-05-09 18:40:56 -0400

    Moving elasticsearch provider to an external plugin

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='pintail-elasticsearch',
+    version='0.2',
+    description='Use Elasticsearch on Pintail sites.',
+    packages=['pintail', 'pintail.elasticsearch'],
+    namespace_packages=['pintail'],
+    install_requires=['pintail>=0.2', 'elasticsearch'],
+    author='Shaun McCance',
+    author_email='shaunm@gnome.org',
+    license='GPLv2+',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Text Processing :: Markup',
+        'Topic :: Text Processing :: Markup :: XML',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)'
+    ],
+)
